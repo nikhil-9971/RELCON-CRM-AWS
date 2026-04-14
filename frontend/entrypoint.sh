253dd386d33fdf67705f24d16e6caef526b9ff5a
Nikhil Trivedi <nikhiltrivedi9971@gmail.com>
2026-04-15 01:26:03 +0530

    Fix : Banckend url updated

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BACKEND_URL=${BACKEND_URL:-"https://relcon-crm.onrender.com"}
+BACKEND_URL=${BACKEND_URL:-"/api"}
 sed -i "s|%%BACKEND_URL%%|${BACKEND_URL}|g" /usr/share/nginx/html/config.js
 echo "Backend URL: $BACKEND_URL"
 exec nginx -g "daemon off;"
